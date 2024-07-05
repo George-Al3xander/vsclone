6ac62d8385dde9f24339dd19046e19152f6cc369
George <george.valuiskyi@gmail.com>
2024-07-05 08:10:13 +0300

    feat: add Next.js convention files
    Add error, loading, and not-found Next.js convention files.

---
+++ src/app/error.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { HiEmojiSad } from "react-icons/hi";
+function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <section className="flex h-screen w-screen flex-col items-center justify-center gap-4 p-4">
+      <HiEmojiSad size={150} />
+      <h2 className="text-4xl font-bold">Oops! Something went wrong.</h2>
+      <p className={"text-2xl opacity-60"}>
+        We&apos;re sorry, but it seems there has been an error:
+      </p>
+      <p className={"opacity-60"}>{error.message}</p>
+      <Button variant={"outline"} onClick={() => reset()}>
+        Try again
+      </Button>
+    </section>
+  );
+}
+
+export default Error;
