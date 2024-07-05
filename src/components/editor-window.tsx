d41e01a9af89e009982ce29b58d961bb5d88b4ff
George <george.valuiskyi@gmail.com>
2024-07-05 09:17:53 +0300

    refactor: move editor logic into a custom hook
    Move editor logic into a custom hook including editor options, loading
    component, value, default value, onChange & onMount functions and pass it
    all using the spread operator.

--- src/components/editor-window.tsx
+++ src/components/editor-window.tsx
@@ -1,64 +1,28 @@
 "use client";
-import React, { useRef } from "react";
+import React from "react";
 import Editor from "@monaco-editor/react";
-import useGetCurrLang from "@/hooks/useGetCurrLang";
-import { VscLoading } from "react-icons/vsc";
-import { useRecoilState, useRecoilValue } from "recoil";
-import {
-  $currentCode,
-  $intelliSenseStatus,
-  $outputPosition,
-  $outputVisibility,
-} from "@/state/atoms/atoms";
-import { debounce } from "lodash";
 import { cn } from "@/lib/utils";
-import { roundedGlassStyle, sampleCode } from "@/constants/data";
-import { $isVertical } from "@/state/selectors/selectors";
-import LoadingMessage from "@/components/loading-message";
+import { roundedGlassStyle } from "@/constants/data";
+import useEditor from "@/hooks/use-editor";
 function EditorWindow() {
-  const editorRef = useRef<unknown | null>(null);
-  const currentLanguage = useGetCurrLang();
-  const [currentCode, setCurrentCode] = useRecoilState($currentCode);
-  if (currentCode === undefined) {
-    setCurrentCode(sampleCode[currentLanguage]);
-  }
-
-  const handleChange = debounce((value?: string) => {
-    setCurrentCode(value || "");
-  }, 500);
-  const onMount = (editor: { focus: () => void }) => {
-    editorRef.current = editor;
-    editor.focus();
-  };
-  const outputVisibility = useRecoilValue($outputVisibility);
-  const intelliSenseStatus = useRecoilValue($intelliSenseStatus);
-  const isVertical = useRecoilValue($isVertical);
+  const { isVertical, outputVisibility, ...props } = useEditor();
 
   return (
     <div
       className={cn("p-2", roundedGlassStyle, {
-        "sm:w-[70%]": !isVertical,
+        "sm:w-[70%]": !isVertical && outputVisibility,
+        "sm:w-[100%]": !outputVisibility,
       })}
     >
       <Editor
-        className={cn("h-[calc(100vh-12rem)] sm:h-[calc(100vh-9.5rem-20vh)]", {
-          "sm:h-[calc(100vh-9rem)]": !outputVisibility || !isVertical,
-        })}
+        {...props}
+        className={cn(
+          "z-20 h-[calc(100vh-12rem)] w-full sm:h-[calc(100vh-9.5rem-20vh)]",
+          {
+            "sm:h-[calc(100vh-9rem)]": !outputVisibility || !isVertical,
+          },
+        )}
         theme="vs-dark"
-        onChange={handleChange}
-        value={currentCode}
-        options={{
-          automaticLayout: true,
-          quickSuggestions: intelliSenseStatus,
-        }}
-        onMount={onMount}
-        language={currentLanguage}
-        loading={
-          <LoadingMessage
-            outputVisibility={outputVisibility}
-            isVertical={isVertical}
-          />
-        }
       />
     </div>
   );
---
+++ src/hooks/use-editor.ts
@@ -0,0 +1,56 @@
+import React, { useRef } from "react";
+import useGetCurrLang from "@/hooks/use-get-curr-lang";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  $currentCode,
+  $intelliSenseStatus,
+  $outputVisibility,
+} from "@/state/atoms/atoms";
+import { debounce } from "lodash";
+import { $isVertical } from "@/state/selectors/selectors";
+import { sampleCode } from "@/constants/data";
+import LoadingMessage from "@/components/loading-message";
+
+const useEditor = () => {
+  const editorRef = useRef<unknown | null>(null);
+
+  const currentLanguage = useGetCurrLang();
+  const [currentCode, setCurrentCode] = useRecoilState($currentCode);
+
+  const outputVisibility = useRecoilValue($outputVisibility);
+  const intelliSenseStatus = useRecoilValue($intelliSenseStatus);
+  const isVertical = useRecoilValue($isVertical);
+
+  const handleChange = debounce((value?: string) => {
+    setCurrentCode(value || "");
+  }, 500);
+
+  const onMount = (editor: { focus: () => void }) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
+
+  const options = {
+    automaticLayout: true,
+    quickSuggestions: intelliSenseStatus,
+  };
+
+  const loading = React.createElement(LoadingMessage, {
+    outputVisibility,
+    isVertical,
+  });
+
+  return {
+    onMount,
+    onChange: handleChange,
+    value: currentCode,
+    language: currentLanguage,
+    defaultValue: currentCode ?? sampleCode[currentLanguage],
+    isVertical,
+    outputVisibility,
+    options,
+    loading,
+  };
+};
+
+export default useEditor;
