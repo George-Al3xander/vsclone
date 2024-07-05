86df2652c0a84958177c9e6c7205dcc90df92aa9
George <george.valuiskyi@gmail.com>
2024-07-05 08:18:36 +0300

    feat: add alert dialog component
    Add an alert dialog component for different file extensions after import.

---
+++ src/components/extension-alert-dialog.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import useExtensionDialog from "@/hooks/use-extension-dialog";
+import { fileExtensionDialog } from "@/constants/data";
+
+const { title, description, btn_cancel, btn_accept } = fileExtensionDialog;
+
+export default function ExtensionAlertDialog() {
+  const { open, accept, setOpen, cancel } = useExtensionDialog();
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={cancel}>{btn_cancel}</AlertDialogCancel>
+          <AlertDialogAction onClick={accept}>{btn_accept}</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
