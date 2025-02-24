c0ca77231eeb4b72b0dfc3a608abfb6765487ea3
George <george.valuiskyi@gmail.com>
2025-02-24 17:17:16 +0200

    feat: config -> languages & metadata
    Add config files for programming languages and metadata.

---
+++ src/configs/metadata.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+
+export const metadataConfig: Metadata = {
+    title: "VSClone",
+    description: "Effortless web-based code compiler",
+    authors: { name: "Heorhii Valuiskyi" },
+};
