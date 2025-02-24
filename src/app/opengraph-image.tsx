1b68164d9cd045b841eccdaaf4b6547c62b41624
George <george.valuiskyi@gmail.com>
2025-02-24 17:38:08 +0200

    feat: og image
    Implement the open graph image and VSCode raw svg icon.

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,84 @@
+import { LANGUAGES_CONFIG } from "@/configs/languages";
+import { metadataConfig } from "@/configs/metadata";
+import { VsCodeIconRaw } from "@/ui/icons";
+import { getLocalFont } from "@/utils/get-local-font";
+import { ImageResponse } from "next/og";
+
+export const alt = `Online Code Compiler (Editor) - ${metadataConfig.title}`;
+
+export const size = {
+    width: 1200,
+    height: 630,
+};
+const LANGUAGES = Object.keys(LANGUAGES_CONFIG);
+
+export const contentType = "image/png";
+const { title } = metadataConfig;
+
+export default async function Image() {
+    const inter = await getLocalFont();
+
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: "1rem 2rem",
+                    justifyItems: "center",
+                    alignItems: "center",
+                    color: "white",
+                    background:
+                        "radial-gradient(circle at 10% 20%, rgb(3, 235, 255) 0%, rgb(152, 70, 242) 100.2%)",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "1rem",
+                        marginTop: "auto",
+                    }}
+                >
+                    <VsCodeIconRaw />
+                    <div style={{ fontSize: 180 }}>{title?.toString()}</div>
+                </div>
+                <div
+                    style={{
+                        fontSize: 80,
+                        opacity: 0.7,
+                        marginBottom: "auto",
+                    }}
+                >
+                    Online Compiler
+                </div>
+
+                <div
+                    style={{
+                        display: "flex",
+                        gap: "2rem",
+                        opacity: 0.7,
+                        marginTop: "auto",
+                        fontSize: 30,
+                    }}
+                >
+                    {LANGUAGES.map((lang) => (
+                        <div key={lang}>
+                            {
+                                LANGUAGES_CONFIG[
+                                    lang as keyof typeof LANGUAGES_CONFIG
+                                ].title
+                            }
+                        </div>
+                    ))}
+                </div>
+            </div>
+        ),
+        {
+            ...size,
+            fonts: [inter],
+        },
+    );
+}
