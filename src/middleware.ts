296650da04f3a4cc087bb4c95d19db4b07e4d665
George <george.valuiskyi@gmail.com>
2025-02-24 17:18:30 +0200

    feat: middleware
    Add the middleware config to validate the programming language in the
    pathname.

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { LANGUAGES_CONFIG } from "@/configs/languages";
+import { NextRequest, NextResponse } from "next/server";
+
+const LANGUAGES = Object.keys(LANGUAGES_CONFIG);
+
+export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+    const language = pathname.split("/")[1] || "";
+
+    if (!LANGUAGES.includes(language)) {
+        return NextResponse.redirect(new URL("/javascript", request.url));
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/((?!api|_next/static|_next/image|favicon.ico|opengraph-image).*)",
+    ],
+};
