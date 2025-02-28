ae24fe1805ce66b703eaa36ca80d50bc325ea3d8
George <george.valuiskyi@gmail.com>
2025-02-28 14:35:03 +0200

    feat: zustand -> code store
    Add Zustand library and create a use-code-store.ts file.

---
+++ src/store/use-code-store.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export type CodeState = {
+    code: string;
+    compilationOutput: string | null;
+    isCompiling: boolean;
+} & {
+    setCode: (code: string) => void;
+    setCompilationOutput: (output: string | null) => void;
+    setIsCompiling: (bool: boolean) => void;
+    clearCompilationOutput: () => void;
+};
+
+export const useCodeStore = create<CodeState>()(
+    persist(
+        (set) => ({
+            code: "",
+            compilationOutput: null,
+            isCompiling: false,
+            setCode: (code) => set(() => ({ code })),
+            setCompilationOutput: (compilationOutput) =>
+                set(() => ({ compilationOutput })),
+            setIsCompiling: (isCompiling) => set(() => ({ isCompiling })),
+            clearCompilationOutput: () =>
+                set(() => ({ compilationOutput: null })),
+        }),
+        {
+            name: "code-storage",
+            partialize: (state) => ({ code: state.code }),
+        },
+    ),
+);
