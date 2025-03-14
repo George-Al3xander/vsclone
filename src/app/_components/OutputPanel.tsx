93014895a9915b103cb4360c19b696f8ee8f58b3
George <george.valuiskyi@gmail.com>
2025-03-14 17:30:14 +0200

    refactor: output panel
    Modify the OutputPanel to have separate props for both the ScrollArea
    and the div wrapper.

--- src/app/_components/OutputPanel.tsx
+++ src/app/_components/OutputPanel.tsx
@@ -5,35 +5,52 @@ import { useCodeStore } from "@/store/use-code-store";
 import { Button } from "@/ui/components/atoms/Button";
 import { ScrollArea } from "@/ui/components/atoms/ScrollArea";
 import { RxCross1Icon } from "@/ui/icons";
+import { cn } from "@/utils/cn";
+import { ComponentProps, FC, HTMLAttributes } from "react";
 
-export const OutputPanel = () => {
+type Props = {
+    wrapperProps?: HTMLAttributes<HTMLDivElement>;
+    scrollAreaProps?: ComponentProps<typeof ScrollArea>;
+};
+
+export const OutputPanel: FC<Props> = ({ wrapperProps, scrollAreaProps }) => {
     const output = useCodeStore((s) => s.compilationOutput);
-    const setOutput = useCodeStore((s) => s.setCompilationOutput);
-    const clearOutput = () => setOutput(null);
+    const clearCompilationOutput = useCodeStore(
+        (s) => s.clearCompilationOutput,
+    );
+
+    const lines: string[] | null = output?.split("\n").filter(Boolean) || null;
 
     return (
         <div
-            className={
-                "roundedGlass relative z-[1] mt-auto flex h-[calc(100vh-10rem)] w-full flex-col gap-4 p-4 sm:h-[20vh] sm:flex-row-reverse"
-            }
+            {...wrapperProps}
+            className={cn(
+                "roundedGlass relative z-[1] box-border flex h-[calc(100vh-10rem)] w-full flex-col gap-4 p-4 sm:h-full sm:flex-row-reverse",
+                wrapperProps?.className,
+            )}
         >
-            <div
-                className={
-                    "ml-auto h-[max-content] w-[max-content] rounded-md bg-primary px-2"
-                }
-            >
-                <Button variant="ghost" onClick={clearOutput} size="icon">
+            <div className="ml-auto h-[max-content] w-[max-content] rounded-md bg-primary px-2">
+                <Button
+                    variant="ghost"
+                    onClick={clearCompilationOutput}
+                    size="icon"
+                >
                     <span className="sr-only">Clear output</span>
                     <RxCross1Icon size={20} />
                 </Button>
                 <ExecuteCodeButton />
             </div>
-            <ScrollArea className={"h-[calc(20vh-4rem)] flex-1 p-2"}>
+            <ScrollArea
+                {...scrollAreaProps}
+                className={cn(
+                    "my-auto flex-1 sm:h-[calc(20vh-4rem)]",
+                    scrollAreaProps?.className,
+                )}
+            >
                 <p className="flex flex-col break-all">
-                    {output
-                        ?.split("\n")
-                        .filter(Boolean)
-                        .map((line) => <span key={line}>{line}</span>) ||
+                    {lines?.map((l, i) => (
+                        <span key={"output-line-" + i + l}>{l}</span>
+                    )) ||
                         "Write, Edit and Run your code using online compiler."}
                 </p>
             </ScrollArea>
