4e3b9ce3ba1a8c8f3b03dbc72164990c9e8fedd7
George <george.valuiskyi@gmail.com>
2025-03-15 13:16:19 +0200

    chore: add github repo url env var
    Update the env.sample file and the environment configuration to include
    the GitHub repository URL as an environment variable

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -4,14 +4,12 @@ import { z } from "zod";
 export const env = createEnv({
     server: {
         NODE_ENV: z.enum(["development", "test", "production"]),
-        BASE_URL: z.string().url(),
     },
     client: {
-        NEXT_PUBLIC_BASE_URL: z.string().url(),
+        NEXT_PUBLIC_GITHUB_URL: z.string().url(),
     },
     runtimeEnv: {
         NODE_ENV: process.env.NODE_ENV,
-        BASE_URL: process.env.BASE_URL,
-        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+        NEXT_PUBLIC_GITHUB_URL: process.env.NEXT_PUBLIC_GITHUB_URL,
     },
 });
