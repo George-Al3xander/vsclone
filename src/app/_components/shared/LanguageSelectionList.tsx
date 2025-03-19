f9c2f3e5fa10a3bd07c2e0e7cef64f66988958db
George <george.valuiskyi@gmail.com>
2025-03-19 10:45:02 +0200

    feat: languages hook
    Create the useLanguages hook and refactor the LanguageSelectionList
    component to use it.

--- src/app/_components/shared/LanguageSelectionList.tsx
+++ src/app/_components/shared/LanguageSelectionList.tsx
@@ -1,70 +1,37 @@
 "use client";
 
-import { useLanguageParam } from "@/app/_hooks/use-language-param";
-import { LANGUAGES_CONFIG } from "@/configs/languages";
-import {
-    CppIcon,
-    CSharpIcon,
-    JavaIcon,
-    JavaScriptIcon,
-    PhpIcon,
-    PythonIcon,
-    TypeScriptIcon,
-} from "@/ui/icons";
+import { useLanguages } from "@/app/_hooks/use-languages";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
-export const ICONS = {
-    javascript: JavaScriptIcon,
-    typescript: TypeScriptIcon,
-    python: PythonIcon,
-    java: JavaIcon,
-    csharp: CSharpIcon,
-    php: PhpIcon,
-    cpp: CppIcon,
-} as const;
-
 export const LanguageSelectionList = () => {
-    const currentLanguage = useLanguageParam();
+    const languages = useLanguages();
 
     return (
-        <ul
-            className={cn(
-                "flex flex-col gap-4 py-4",
-                "sm:h-full sm:justify-between sm:p-2",
-            )}
-        >
-            {Object.entries(LANGUAGES_CONFIG).map(
-                ([lang, { title, version }]) => {
-                    const Icon = ICONS[lang as keyof typeof LANGUAGES_CONFIG];
-                    const isActive = Boolean(currentLanguage === lang);
+        <ul className="flex h-full flex-col justify-between gap-4 p-2">
+            {languages.map(
+                ({ language, title, version, isCurrent, icon: Icon }) => (
+                    <li key={language}>
+                        <Link
+                            title={`${title} ${version}`}
+                            href={"/" + language}
+                            target="_blank"
+                            className={cn(
+                                "flex max-w-[4rem] scale-75 items-center gap-2 rounded border-white p-2 opacity-40 transition-all hover:scale-100 hover:cursor-pointer hover:border-2 hover:opacity-100",
 
-                    return (
-                        <li key={lang}>
-                            <Link
-                                title={`${title} ${version}`}
-                                href={"/" + lang}
-                                target="_blank"
-                                className={cn(
-                                    "flex items-center gap-2 border-white opacity-60 transition-all hover:cursor-pointer hover:border-r-4 hover:opacity-100",
-                                    "hover:opacity-100 sm:max-w-[4rem] sm:scale-75 sm:rounded sm:p-2 sm:opacity-40 hover:sm:scale-100 hover:sm:border-2",
-                                    {
-                                        "pointer-events-none border-r-4 border-white opacity-100 sm:scale-100 sm:border-2 sm:opacity-100":
-                                            isActive,
-                                    },
-                                )}
-                            >
-                                <Icon className="size-[20px] sm:mx-auto sm:size-[35px]" />
-                                <span className="sm:hidden">
-                                    {title} ({version})
-                                </span>
-                                <span className="sr-only">
-                                    Go to {title} compiler
-                                </span>
-                            </Link>
-                        </li>
-                    );
-                },
+                                {
+                                    "pointer-events-none scale-100 border-2 opacity-100":
+                                        isCurrent,
+                                },
+                            )}
+                        >
+                            <Icon className="size-[20px] sm:mx-auto sm:size-[35px]" />
+                            <span className="sr-only">
+                                Go to {title} compiler
+                            </span>
+                        </Link>
+                    </li>
+                ),
             )}
         </ul>
     );
---
+++ src/app/_hooks/use-languages.ts
@@ -0,0 +1,37 @@
+import { useLanguageParam } from "@/app/_hooks/use-language-param";
+import { LANGUAGES_CONFIG } from "@/configs/languages";
+import {
+    CppIcon,
+    CSharpIcon,
+    JavaIcon,
+    JavaScriptIcon,
+    PhpIcon,
+    PythonIcon,
+    TypeScriptIcon,
+} from "@/ui/icons";
+import { IconType } from "react-icons";
+
+export const ICONS = {
+    javascript: JavaScriptIcon,
+    typescript: TypeScriptIcon,
+    python: PythonIcon,
+    java: JavaIcon,
+    csharp: CSharpIcon,
+    php: PhpIcon,
+    cpp: CppIcon,
+} as const;
+
+export const useLanguages = (): ({
+    language: string;
+    isCurrent: boolean;
+    icon: IconType;
+} & (typeof LANGUAGES_CONFIG)[keyof typeof LANGUAGES_CONFIG])[] => {
+    const currentLanguage = useLanguageParam();
+
+    return Object.entries(LANGUAGES_CONFIG).map(([lang, fields]) => {
+        const icon = ICONS[lang as keyof typeof LANGUAGES_CONFIG];
+        const isCurrent = Boolean(currentLanguage === lang);
+
+        return { language: lang, icon, isCurrent, ...fields };
+    });
+};
