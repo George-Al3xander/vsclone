5ae294c6e936c75444fc9b052f15c6ea8e5ef17e
George <george.valuiskyi@gmail.com>
2025-03-20 10:25:01 +0200

    refactor: language selection list -> scroll area
    Wrap the LanguageSelectionList with the scroll area.

--- src/app/_components/shared/LanguageSelectionList.tsx
+++ src/app/_components/shared/LanguageSelectionList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useLanguages } from "@/app/_hooks/use-languages";
+import { ScrollArea } from "@/ui/components/atoms/ScrollArea";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
@@ -8,31 +9,33 @@ export const LanguageSelectionList = () => {
     const languages = useLanguages();
 
     return (
-        <ul className="flex h-full flex-col justify-between gap-4 p-2">
-            {languages.map(
-                ({ language, title, version, isCurrent, icon: Icon }) => (
-                    <li key={language}>
-                        <Link
-                            title={`${title} ${version}`}
-                            href={"/" + language}
-                            target="_blank"
-                            className={cn(
-                                "flex max-w-[4rem] scale-75 items-center gap-2 rounded border-white p-2 opacity-40 transition-all hover:scale-100 hover:cursor-pointer hover:border-2 hover:opacity-100",
+        <ScrollArea className="h-[calc(100vh-8rem)] [&_div]:h-full">
+            <ul className="flex h-full flex-col justify-between gap-4 p-2">
+                {languages.map(
+                    ({ language, title, version, isCurrent, icon: Icon }) => (
+                        <li key={language}>
+                            <Link
+                                title={`${title} ${version}`}
+                                href={"/" + language}
+                                target="_blank"
+                                className={cn(
+                                    "flex max-w-[4rem] scale-75 items-center gap-2 rounded border-white p-2 opacity-40 transition-all hover:scale-100 hover:cursor-pointer hover:border-2 hover:opacity-100",
 
-                                {
-                                    "pointer-events-none scale-100 border-2 opacity-100":
-                                        isCurrent,
-                                },
-                            )}
-                        >
-                            <Icon className="size-[20px] sm:mx-auto sm:size-[35px]" />
-                            <span className="sr-only">
-                                Go to {title} compiler
-                            </span>
-                        </Link>
-                    </li>
-                ),
-            )}
-        </ul>
+                                    {
+                                        "pointer-events-none scale-100 border-2 opacity-100":
+                                            isCurrent,
+                                    },
+                                )}
+                            >
+                                <Icon className="size-[20px] sm:mx-auto sm:size-[35px]" />
+                                <span className="sr-only">
+                                    Go to {title} compiler
+                                </span>
+                            </Link>
+                        </li>
+                    ),
+                )}
+            </ul>
+        </ScrollArea>
     );
 };
